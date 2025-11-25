2e77a407fcebc667cfeea56c27362bc3decd7dd7
irisfofs <iris@ink.horse>
2025-11-24 16:22:15 -0800

    Rename KubeJS tweak file

--- kubejs/server_scripts/src/fofs_tweaks.js
+++ kubejs/server_scripts/src/fofs_tweaks.js
@@ -1,8 +1,10 @@
+// Custom recipes and tags that don't fit better anywhere else.
+
 ServerEvents.recipes((event) => {
   // Wash magma cream to make it less spicy.
   event.recipes.create
     .splashing("minecraft:slime_ball", "minecraft:magma_cream")
-    .id("hauser:unspicy_magma_cream");
+    .id("fofs:unspicy_magma_cream");
 });
 
 // Custom tag for Jade equipment requirement.
