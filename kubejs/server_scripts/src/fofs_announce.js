59ba95f63ce99e762576b03eb18172f451a23726
irisfofs <iris@ink.horse>
2026-01-02 13:20:06 -0800

    Update announcement script to read an array of messages

--- kubejs/server_scripts/src/fofs_announce.js
+++ kubejs/server_scripts/src/fofs_announce.js
@@ -3,52 +3,6 @@ PlayerEvents.loggedIn((event) => {
   announceToPlayer(event.player);
 });
 
-function formatTitle(date, title) {
-  return `<yellow>[${date}]</yellow> <bold><gold>${title}</gold></bold>`;
-}
-
-function announceToPlayer(player) {
-  const title = formatTitle("2025-12-16", "1st Photo Contest");
-  player.tell(
-    global.parseTextFormat(`${title}
-<bold>Theme:</bold> "Welcome to my house !!"
-<bold>Due:</bold> 2025-12-31 (end of day)
-
-<bold><gold>To Enter</gold></bold>
-- Submit one (1) photo via the hopper at Iris's base (no peeking).
-  - For bonus points*, put it in a Present first. (*bonus points are not real)
- 
-<bold><gold>Prizes</gold></bold>
-- All entrants will receive 1 Interplanar Projector, which you can use to import a meme of your choice as a photo. (Or a picture, whatever.)
-- Other prizes (and judging details) to be announced later!
-
-<bold>Tip:</bold> An Instant Camera is a lot easier to get started with, but takes lower-res pictures.
-
-${title} (open chat and scroll up to read)
-`)
-  );
-
-  player.tell(
-    global.parseTextFormat(`${formatTitle(
-      "2025-12-31",
-      "Contest #1 Deadline Extension"
-    )}
-Due to busy schedules on New Year's Eve, the new deadline is <bold>2026-01-01</bold>.`)
-  );
-}
-
-/*
-## 1st Photo Contest
-Theme: "Welcome to my house !!"
-Submission deadline: 2025-12-31 23:59
-
-- Submit one (1) photo via the hopper at Iris's base (no peeking).
-- All entrants will receive 1 Interplanar Projector, which you can use to import a meme of your choice as a photo. (Or a picture, whatever.)
-- Other prizes (and judging details) to be announced later!
-
-(an Instant Camera is a lot easier to get started with than the normal one)
-*/
-
 ServerEvents.commandRegistry((event) => {
   const { commands: Commands, arguments: Arguments } = event;
 
@@ -61,3 +15,56 @@ ServerEvents.commandRegistry((event) => {
       })
   );
 });
+
+function formatTitle(date, title) {
+  return `<yellow>[${date}]</yellow> <bold><gold>${title}</gold></bold>`;
+}
+
+function announceToPlayer(player) {
+  let lastTitle = null;
+  for (const message of MESSAGES.filter((m) => m.show)) {
+    let { title, date, body } = message;
+    let formattedTitle = formatTitle(date, title);
+    player.tell(global.parseTextFormat(`${formattedTitle}\n${body}`));
+    lastTitle = formattedTitle;
+  }
+
+  // Repeat the last message title at the end.
+  if (lastTitle) {
+    player.tell(
+      global.parseTextFormat(`${lastTitle} (open chat and scroll up to read)`)
+    );
+  }
+}
+
+const MESSAGES = [
+  {
+    show: false,
+    date: "2025-12-16",
+    title: "1st Photo Contest",
+    body: `<bold>Theme:</bold> "Welcome to my house !!"
+<bold>Due:</bold> 2025-12-31 (end of day)
+
+<bold><gold>To Enter</gold></bold>
+- Submit one (1) photo via the hopper at Iris's base (no peeking).
+  - For bonus points*, put it in a Present first. (*bonus points are not real)
+ 
+<bold><gold>Prizes</gold></bold>
+- All entrants will receive 1 Interplanar Projector, which you can use to import a meme of your choice as a photo. (Or a picture, whatever.)
+- Other prizes (and judging details) to be announced later!
+
+<bold>Tip:</bold> An Instant Camera is a lot easier to get started with, but takes lower-res pictures.`,
+  },
+  {
+    show: false,
+    date: "2025-12-31",
+    title: "Contest #1 Deadline Extension",
+    body: `Due to busy schedules on New Year's Eve, the new deadline is <bold>2026-01-01</bold>.`,
+  },
+  {
+    show: true,
+    date: "2026-01-02",
+    title: "Contest #1 Submissions Closed!",
+    body: `We had eight (8) people enter! Thanks for your submissions, everyone. Judging in progress and results will be returned soon.`,
+  },
+];
