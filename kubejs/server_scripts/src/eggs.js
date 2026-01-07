a2b819163823d9993525ab5f603911ab65be5577
irisfofs <iris@ink.horse>
2026-01-06 16:49:40 -0800

    Allow frying and boiling all bird eggs
    Also fix two other recipes (vanilla pudding and raw ube cake base) that
    only used `minecraft:egg`.
    
    Also fix an issue where you couldn't peel a boiled egg (it powdered it
    instead).

--- kubejs/server_scripts/src/eggs.js
+++ kubejs/server_scripts/src/eggs.js
@@ -1,23 +1,69 @@
 ServerEvents.tags("item", (event) => {
-  event.add("forge:eggs/bird", "untitledduckmod:goose_egg");
-  event.add("forge:eggs/bird", "untitledduckmod:duck_egg");
-  event.add("c:bread_slices", "createfood:toast_slice");
+  // Only allow powdering peeled boiled eggs, since otherwise you can't peel
+  // them with a cutting board.
+  event.add("c:powderable_eggs", "createfood:boiled_egg_peeled");
+  event.remove("c:powderable_eggs", "createfood:boiled_egg");
 });
 
-// ServerEvents.recipes((event) => {
-//   // Replace this cooking recipe with one that uses the tag.
-//   event.remove({ id: "culturaldelights:cooking/raw_pasta" });
-//   event
-//     .custom({
-//       type: "farmersdelight:cooking",
-//       ingredients: [
-//         { item: "minecraft:wheat" },
-//         { item: "minecraft:wheat" },
-//         { tag: "forge:eggs/bird" },
-//       ],
-//       result: { item: "farmersdelight:raw_pasta", count: 4 },
-//       experience: 0.2,
-//       cookingtime: 200,
-//     })
-//     .id("culturaldelights:cooking/raw_pasta");
-// });
+ServerEvents.recipes((event) => {
+  // Allow only actual eggs.
+  event.replaceInput(
+    { output: "farmersdelight:fried_egg" },
+    "minecraft:egg",
+    "#c:eggs/bird"
+  );
+
+  // Allow using all eggs and egg substitutes.
+  event.replaceInput(
+    { output: "neapolitan:vanilla_pudding" },
+    "minecraft:egg",
+    "#c:eggs"
+  );
+  event.replaceInput(
+    { output: "createfood:raw_ube_cake_base" },
+    "minecraft:egg",
+    "#c:eggs"
+  );
+
+  // Manually recreate Create and Farmer's Delight cooking recipes, since
+  // replaceInput doesn't work on them.
+  event.remove({
+    id: "createfood:create/mixing/boiled_egg_from_mixing_heated_water",
+  });
+  event.recipes.create
+    .mixing("createfood:boiled_egg", [
+      { tag: "c:eggs/bird" },
+      Fluid.of("minecraft:water", 125),
+      { tag: "c:salt" },
+    ])
+    .heated()
+    .id("fofs:createfood/create/mixing/boiled_egg_from_mixing_heated_water");
+
+  event.remove({
+    id: "createfood:farmersdelight/cooking/boiled_egg_from_cooking",
+  });
+  event
+    .custom({
+      type: "farmersdelight:cooking",
+      container: { count: 1, id: "minecraft:stick" },
+      experience: 0.2,
+      cookingtime: 200,
+      ingredients: [
+        { item: "minecraft:water_bucket" },
+        { tag: "c:eggs/bird" },
+        { tag: "c:salt" },
+      ],
+      recipe_book_tab: "misc",
+      result: { count: 1, id: "createfood:boiled_egg" },
+      "neoforge:conditions": [
+        {
+          type: "almostunified:conditional",
+          conditions_met: true,
+          original_conditions: [
+            { type: "createfood:enabled", id: "boiled_egg" },
+          ],
+        },
+      ],
+    })
+    .id("fofs:createfood/farmersdelight/cooking/boiled_egg_from_cooking");
+});
